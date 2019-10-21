ea00359b9a88fe159c8d2be2daa30542480e09b4
eliassaliba12 <54477920+eliassaliba12@users.noreply.github.com>
2019-10-21 15:02:42 +0300

    Update Main.php

--- src/Elias/FallNo/Main.php
+++ src/Elias/FallNo/Main.php
@@ -1,24 +1,22 @@
 <?php
-
 namespace Elias\FallNo;
-
 use pocketmine\plugin\PluginBase;
 use pocketmine\Player;
 use pocketmine\event\Listener;
 use pocketmine\event\entity\EntityDamageEvent;
-
 class Main extends PluginBase implements Listener {
-
 public function onEnable() {
  $this->getLogger()->info("your plugin started");
  $this->getServer()->getPluginManager()->registerevents(($this), $this);
  $this->saveResource("config.yml");
 }
-public function onFall(EntityDamageEvent $event) {
+ public function onFall(EntityDamageEvent $event) {
+  if(in_array($event->getPlayer()->getLevel()->getFolderName(), Main::get()->getConfig()->get("worlds"))){
+      $event->setCancelled(false);
+   return false;
+ }
  if($event->getCause() === EntityDamageEvent::CAUSE_FALL) {
   $event->setCancelled();// cancels the event
- }elseif(in_array($event->getPlayer()->getLevel()->getFolderName(), Main::get()->getConfig()->get("worlds"))){
-$event->setCancelled(false);
-}
-}
+ }
+ }
 }
